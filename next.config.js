778669a434ab679b43d39e751ed0542b9c54ac52
nadoor6 <nadernader777111@gmail.com>
2025-11-09 20:10:49 +0300

    edit

--- next.config.js
+++ next.config.js
@@ -1,17 +1,22 @@
+// next.config.js
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [{
-                protocol: 'https',
-                hostname: 'firebasestorage.googleapis.com',
-            },
-            {
-                protocol: 'http',
-                hostname: 'localhost',
-            },
-        ],
-        unoptimized: true,
+    // ... your other config
+    output: 'export', // Keep this if you need static export
+    webpack: (config) => {
+        config.plugins.push(
+            new CopyPlugin({
+                patterns: [{
+                    from: path.join(__dirname, '.next/routes-manifest.json'),
+                    to: path.join(__dirname, 'out/'),
+                }, ],
+            })
+        );
+        return config;
     },
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
